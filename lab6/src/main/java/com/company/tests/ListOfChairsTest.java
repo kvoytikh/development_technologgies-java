66a0936141484c3bbcd6a297e4b333c1797e8a60
kvoytikh <61868920+kvoytikh@users.noreply.github.com>
2020-12-28 19:26:28 +0200

    Add files via upload

---
+++ lab6/src/main/java/com/company/tests/ListOfChairsTest.java
@@ -0,0 +1,42 @@
+package com.company.tests;
+
+import com.company.java.Brand;
+import com.company.java.Chair;
+import com.company.java.ListOfChairs;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListOfChairsTest {
+    ListOfChairs list = new ListOfChairs();
+    Chair ourChair1 = new Chair(Brand.AMF, 100);
+
+
+    @Test
+    void testAddChair() {
+        assertEquals(ListOfChairs.addChair(ourChair1).size(), 1);
+    }
+
+    @Test
+    void countAllPriceOfKind() {
+        assertEquals(list.countAllPriceOfKind("AMF"), 100);
+    }
+
+    @Test
+    void findElementWithMaxPrice() {
+        String res = "AMF -> you need: ChairCover(width, length) -> (30.0, 40.0); Legs -> 4; bolts -> 6";
+        assertEquals(list.findElementWithMaxPrice(), res);
+    }
+
+    @Test
+    void findAveragePrice() {
+        assertEquals(list.findAveragePrice(), 100);
+    }
+
+    @Test
+    void groupByPrice() {
+        String res = "{cheap=[AMF -> you need: ChairCover(width, length) -> (30.0, 40.0); Legs -> 4; bolts -> 6]}";
+        assertEquals(list.groupByPrice(), res);
+    }
+}
